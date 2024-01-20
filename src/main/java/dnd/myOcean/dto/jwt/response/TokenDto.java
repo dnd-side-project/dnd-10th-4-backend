beb942b25f759b89d5b77d829e04b3d57fab4277
shinjaewon99 <tlswodnjs99@naver.com>
2024-01-21 00:12:38 +0900

    refactor: setter 대신 빌더 추가

--- src/main/java/dnd/myOcean/dto/jwt/response/TokenDto.java
+++ src/main/java/dnd/myOcean/dto/jwt/response/TokenDto.java
@@ -1,11 +1,13 @@
 package dnd.myOcean.dto.jwt.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@Builder
+@AllArgsConstructor
 public class TokenDto {
-    private String accessToken;
-    private String refreshToken;
+    private final String accessToken;
+    private final String refreshToken;
 }
--- src/main/java/dnd/myOcean/dto/oAuth/response/MemberInfo.java
+++ src/main/java/dnd/myOcean/dto/oAuth/response/MemberInfo.java
@@ -3,14 +3,12 @@ package dnd.myOcean.dto.oAuth.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
 public class MemberInfo {
-    private Long id;
-    private String nickName;
-    private String profileImageUrl;
+    private final Long id;
+    private final String nickName;
+    private final String profileImageUrl;
 }
