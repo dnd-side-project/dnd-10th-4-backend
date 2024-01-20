04cfae9248bb632a920dd074598fc82f66c85344
shinjaewon99 <tlswodnjs99@naver.com>
2024-01-20 09:22:12 +0900

    feat: 필요 어노테이션 추가

--- src/main/java/dnd/myOcean/domain/member/Member.java
+++ src/main/java/dnd/myOcean/domain/member/Member.java
@@ -3,18 +3,23 @@ package dnd.myOcean.domain.member;
 import dnd.myOcean.domain.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@Table(name = "member")
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
 
     @Column
     private String username;
 
-    @Column(nullable = false)
+    @Column
     private String nickname;
 }
 
