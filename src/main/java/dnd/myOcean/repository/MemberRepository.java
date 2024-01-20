6aa888cb36e488ef4e0dfe78f8ef668fcab94c00
shinjaewon99 <tlswodnjs99@naver.com>
2024-01-21 00:51:38 +0900

    feat: username을 통해 Member 찾는 기능 추가

--- src/main/java/dnd/myOcean/repository/MemberRepository.java
+++ src/main/java/dnd/myOcean/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package dnd.myOcean.repository;
 import dnd.myOcean.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 }
