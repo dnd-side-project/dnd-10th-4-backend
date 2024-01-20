4a7e44e79b95bfc9e637d39da03bb6a29d8f1786
shinjaewon99 <tlswodnjs99@naver.com>
2024-01-21 00:54:13 +0900

    feat: OAuth2 필터 추가

--- src/main/java/dnd/myOcean/config/web/WebSecurityConfig.java
+++ src/main/java/dnd/myOcean/config/web/WebSecurityConfig.java
@@ -1,22 +1,40 @@
 package dnd.myOcean.config.web;
 
+import dnd.myOcean.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final MemberService memberService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()).authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                         .requestMatchers("/**"
-                        ).permitAll().anyRequest().authenticated());
+                        ).permitAll().anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/members/login")
+                        .userInfoEndpoint(userInfoEndpointConfig ->
+                                userInfoEndpointConfig.userService(memberService)
+                        )
+                );
+
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
