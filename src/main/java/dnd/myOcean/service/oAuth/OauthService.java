a0d157e3c009fe07fb9b91334f0989a372a56125
shinjaewon99 <tlswodnjs99@naver.com>
2024-01-20 23:43:21 +0900

    remove: 사용하지 않는 클래스 삭제

--- src/main/java/dnd/myOcean/service/oAuth/OauthService.java
+++
@@ -1,7 +0,0 @@
-package dnd.myOcean.service.oAuth;
-
-import dnd.myOcean.dto.oAuth.response.MemberInfo;
-
-public interface OauthService {
-    MemberInfo getMemberInfo(String code) throws Exception;
-}
