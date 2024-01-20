0d50659eeb777fc1c4e4661a0b13c4ddd0aad081
shinjaewon99 <tlswodnjs99@naver.com>
2024-01-21 00:54:39 +0900

    feat: kakao 로그인 후처리 관련 기능 추가

--- src/main/java/dnd/myOcean/service/member/MemberService.java
+++ src/main/java/dnd/myOcean/service/member/MemberService.java
@@ -1,7 +1,30 @@
 package dnd.myOcean.service.member;
 
-import dnd.myOcean.dto.oAuth.response.MemberInfo;
+import dnd.myOcean.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface MemberService {
-    void createMember(MemberInfo memberInfo);
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MemberService extends DefaultOAuth2UserService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        log.info("--------------------------------------");
+        log.info("userRequest: " + userRequest);
+
+        return super.loadUser(userRequest);
+    }
 }
