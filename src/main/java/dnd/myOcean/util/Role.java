db7ef0d5cb286ebe7586490e135fe12c6654860a
shinjaewon99 <tlswodnjs99@naver.com>
2024-01-21 10:42:41 +0900

    feat: 회원 권한 기능 추가

---
+++ src/main/java/dnd/myOcean/util/Role.java
@@ -0,0 +1,12 @@
+package dnd.myOcean.util;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    USER("유저"), ADMIN("관리자");
+
+    private final String authority;
+}
