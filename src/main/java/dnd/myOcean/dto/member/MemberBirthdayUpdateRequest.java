1acb99d8d7aec4961a287cdea47b7caea40a1cdf
Dongwoongkim <rlaehddnd0422@naver.com>
2024-01-23 16:10:28 +0900

    remove : DTO @AllArgsConstructor 제거

--- src/main/java/dnd/myOcean/dto/member/MemberBirthdayUpdateRequest.java
+++ src/main/java/dnd/myOcean/dto/member/MemberBirthdayUpdateRequest.java
@@ -1,12 +1,10 @@
 package dnd.myOcean.dto.member;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class MemberBirthdayUpdateRequest {
 
