cd1c8bf4ed91b49c9ebff3a8729eb58f034b2a9b
Dongwoongkim <rlaehddnd0422@naver.com>
2024-01-26 18:25:08 +0900

    feat : refreshToken을 관리하기 위한 redis 설정

--- src/main/java/dnd/myOcean/config/security/jwt/filter/JwtFilter.java
+++ src/main/java/dnd/myOcean/config/security/jwt/filter/JwtFilter.java
@@ -1,19 +1,13 @@
 package dnd.myOcean.config.security.jwt.filter;
 
-import dnd.myOcean.config.oAuth.kakao.details.KakaoMemberDetails;
 import dnd.myOcean.config.security.jwt.token.TokenService;
-import dnd.myOcean.dto.jwt.response.TokenDto;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -45,54 +39,15 @@ public class JwtFilter extends OncePerRequestFilter {
         }
 
         String accessToken = getAccessToken(request);
-        String refreshToken = getRefreshToken(request);
 
-        if (validateExpire(accessToken) && validateExpire(refreshToken)) {
-            log.info("DID1");
+        // accessToken 만료 X
+        if (validateExpire(accessToken)) {
             SecurityContextHolder.getContext().setAuthentication(tokenService.getAuthentication(accessToken));
-            request.setAttribute(ACCESS_HEADER, accessToken);
-            request.setAttribute(ACCESS_HEADER, refreshToken);
+        }
 
-        } else if (!validateExpire(accessToken) && validateExpire(refreshToken) && tokenService.validateToken(
-                refreshToken)) {
-            log.info("DID2");
-            Authentication authentication = tokenService.getAuthentication(refreshToken);
+        // accessToken 만료 , refreshToken
+        if (!validateExpire(accessToken) && validateExpire(getRefreshToken(request))) {
 
-            KakaoMemberDetails principal = (KakaoMemberDetails) authentication.getPrincipal();
-            String email = principal.getName();
-
-            Collection<? extends GrantedAuthority> authorities = principal.getAuthorities();
-
-            String role = authorities.stream()
-                    .map(GrantedAuthority::getAuthority)
-                    .collect(Collectors.joining(","));
-
-            TokenDto token = tokenService.createToken(email, role);
-
-            SecurityContextHolder.getContext()
-                    .setAuthentication(tokenService.getAuthentication(token.getAccessToken()));
-
-            request.setAttribute(ACCESS_HEADER, token.getAccessToken());
-
-        } else if (validateExpire(accessToken) && !validateExpire(refreshToken) && tokenService.validateToken(
-                accessToken)) {
-            Authentication authentication = tokenService.getAuthentication(accessToken);
-
-            KakaoMemberDetails principal = (KakaoMemberDetails) authentication.getPrincipal();
-            String email = principal.getName();
-
-            Collection<? extends GrantedAuthority> authorities = principal.getAuthorities();
-
-            String role = authorities.stream()
-                    .map(GrantedAuthority::getAuthority)
-                    .collect(Collectors.joining(","));
-
-            TokenDto token = tokenService.createToken(email, role);
-
-            SecurityContextHolder.getContext()
-                    .setAuthentication(tokenService.getAuthentication(token.getAccessToken()));
-
-            request.setAttribute(REFRESH_HEADER, token.getRefreshToken());
         }
 
         filterChain.doFilter(request, response);
