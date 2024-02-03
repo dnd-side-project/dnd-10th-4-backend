bb09c7ff1324101ce091103c60eeb3130cb019ce
shinjaewon99 <tlswodnjs99@naver.com>
2024-02-03 23:47:19 +0900

    feat: 신고 레포지터리 추가

---
+++ src/main/java/dnd/myOcean/domain/report/repository/ReportRepository.java
@@ -0,0 +1,8 @@
+package dnd.myOcean.domain.report.repository;
+
+import dnd.myOcean.domain.report.domain.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+}
