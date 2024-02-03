4a004836dcffe0761e1db8f2c4886fa51464d07d
shinjaewon99 <tlswodnjs99@naver.com>
2024-02-03 23:46:31 +0900

    feat: 신고 엔티티 클래스 추가

---
+++ src/main/java/dnd/myOcean/domain/report/domain/Report.java
@@ -0,0 +1,43 @@
+package dnd.myOcean.domain.report.domain;
+
+import dnd.myOcean.domain.letter.domain.Letter;
+import dnd.myOcean.domain.member.domain.Member;
+import dnd.myOcean.global.common.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_id")
+    private Long id;
+
+    @Column(updatable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reporter_id")
+    private Member reporter;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reported_id")
+    private Member reported;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "letter_id")
+    private Letter letter;
+
+    @Builder
+    public Report(Letter letter, Member reporter, Member reported, String reportContent) {
+        this.reporter = reporter;
+        this.reported = reported;
+        this.letter = letter;
+        this.content = reportContent;
+    }
+}
