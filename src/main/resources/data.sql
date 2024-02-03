03d37e48e6e9bf634ee723eb2884bcb35d3502d9
shinjaewon99 <tlswodnjs99@naver.com>
2024-02-03 23:45:18 +0900

    init: 더미데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,13 +8,14 @@ VALUES ('WORK'),
        ('FAMILY'),
        ('ETC');
 
-INSERT INTO MEMBER(create_Date, update_age_count, update_gender_count, email, nick_name, gender, role, age)
-VALUES (now(), 0, 0, 'kdo0422@nate.com', '낯선 유저1', 'MALE', 'USER', 26),
-       (now(), 0, 0, 'dog@naver.com', '낯선 강아지', 'MALE', 'USER', 20),
-       (now(), 0, 0, 'cat@naver.com', '낯선 고양이', 'MALE', 'USER', 20),
-       (now(), 0, 0, 'pig@naver.com', '낯선 돼지', 'MALE', 'USER', 100),
-       (now(), 0, 0, 'cow@naver.com', '낯선 소', 'MALE', 'USER', 30),
-       (now(), 0, 0, 'chicken@naver.com', '낯선 닭', 'MALE', 'USER', 40);
+INSERT INTO member(create_Date, update_age_count, update_gender_count, email, nick_name, gender, role, age, has_report)
+VALUES (now(), 0, 0, 'kdo0422@nate.com', '낯선 유저1', 'MALE', 'USER', 26, false),
+       (now(), 0, 0, 'dog@naver.com', '낯선 강아지', 'MALE', 'USER', 20, false),
+       (now(), 0, 0, 'cat@naver.com', '낯선 고양이', 'MALE', 'USER', 20, false),
+       (now(), 0, 0, 'pig@naver.com', '낯선 돼지', 'MALE', 'USER', 100, false),
+       (now(), 0, 0, 'cow@naver.com', '낯선 소', 'MALE', 'USER', 30, false),
+       (now(), 0, 0, 'chicken@naver.com', '낯선 닭', 'MALE', 'USER', 40, false),
+       (now(), 0, 0, 'tnlswodnjs99@naver.com', '낯선 사람', 'MALE', 'USER', 40, false);
 
 /*
  1. kdo0422@nate.com => work, course, relationship
@@ -24,7 +25,7 @@ VALUES (now(), 0, 0, 'kdo0422@nate.com', '낯선 유저1', 'MALE', 'USER', 26),
  5. cow@naver.com => study, family
  6. chicken@naver.com => work, love
  */
-INSERT INTO MEMBER_WORRY(member_id, worry_id)
+INSERT INTO member_worry(member_id, worry_id)
 VALUES (1, 1),
        (1, 2),
        (1, 3),
@@ -37,9 +38,10 @@ VALUES (1, 1),
        (5, 6),
        (5, 7),
        (6, 1),
-       (6, 5);
+       (6, 5),
+       (7, 2);
 
-INSERT INTO LETTER(uuid, sender_id, receiver_id, content, create_date, worry_type,
+INSERT INTO letter(uuid, sender_id, receiver_id, content, create_date, worry_type,
                    has_replied, is_delete_by_sender, is_stored, reply_content)
 VALUES ('1', 2, 1, '안녕', now(), 'WORK', true, false, true, '안녕하세요!'),
        ('1', 3, 1, '안녕', now(), 'LOVE', true, false, true, '안녕하세요!'),
@@ -47,6 +49,7 @@ VALUES ('1', 2, 1, '안녕', now(), 'WORK', true, false, true, '안녕하세요!
        ('2', 1, 2, '안녕', now(), 'LOVE', false, false, false, ''),
        ('2', 1, 3, '안녕', now(), 'WORK', false, false, false, ''),
        ('2', 1, 4, '안녕', now(), 'FAMILY', false, false, false, ''),
+       ('2', 4, 7, '안녕', now(), 'FAMILY', false, false, false, ''),
        ('2', 1, 5, '안녕', now(), 'ETC', false, false, false, '');
 
 
