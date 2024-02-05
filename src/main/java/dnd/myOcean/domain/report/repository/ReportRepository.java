3cd61d44cca9b89293a09aacbdf99659729e1826
shinjaewon99 <tlswodnjs99@naver.com>
2024-02-06 08:39:59 +0900

    feat: 신고자와 신고당한자를 찾기위한 find 메소드 추가

--- src/main/java/dnd/myOcean/domain/report/repository/ReportRepository.java
+++ src/main/java/dnd/myOcean/domain/report/repository/ReportRepository.java
@@ -2,7 +2,15 @@ package dnd.myOcean.domain.report.repository;
 
 import dnd.myOcean.domain.report.domain.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    @Query("select r from Report r where r.reporter.id = :reporterId")
+    Optional<Report> findByReporterId(@Param("reporterId") Long reporterId);
 
+    @Query("select r from Report r where r.reported.id = :reportedId")
+    Optional<Report> findByReportedId(@Param("reportedId") Long reportedId);
 }
