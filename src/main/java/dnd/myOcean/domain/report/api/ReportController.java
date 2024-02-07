657a079f8424a85be87422c8f7df9e0d3742ae5e
Dongwoongkim <rlaehddnd0422@naver.com>
2024-02-07 22:44:05 +0900

    remove : 회원 차단 기능 제거

--- src/main/java/dnd/myOcean/domain/report/api/ReportController.java
+++ src/main/java/dnd/myOcean/domain/report/api/ReportController.java
@@ -6,7 +6,11 @@ import dnd.myOcean.global.auth.aop.AssignCurrentMemberId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +19,7 @@ public class ReportController {
 
     private final ReportService reportService;
 
-    @PatchMapping("{letterId}")
+    @PostMapping("/{letterId}")
     @AssignCurrentMemberId
     public ResponseEntity report(@RequestBody ReportSendRequest reportSendRequest,
                                  @PathVariable("letterId") Long letterId) {
--- src/main/java/dnd/myOcean/domain/report/domain/Report.java
+++ src/main/java/dnd/myOcean/domain/report/domain/Report.java
@@ -3,8 +3,22 @@ package dnd.myOcean.domain.report.domain;
 import dnd.myOcean.domain.letter.domain.Letter;
 import dnd.myOcean.domain.member.domain.Member;
 import dnd.myOcean.global.common.base.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Builder
@@ -22,15 +36,17 @@ public class Report extends BaseEntity {
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reporter_id")
+    @JoinColumn(name = "reportera")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Member reporter;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "reported_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Member reported;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "letter_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Letter letter;
-
 }
