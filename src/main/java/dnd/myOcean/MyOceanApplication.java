886fad71aa10400bb1863a5fcf16da0d323d18fb
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-02-11 04:35:16 +0900

    Update MyOceanApplication.java

--- src/main/java/dnd/myOcean/MyOceanApplication.java
+++ src/main/java/dnd/myOcean/MyOceanApplication.java
@@ -15,7 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MyOceanApplication {
 
     public static void main(String[] args) {
-        System.out.println("HI");s
+        System.out.println("HI");
         SpringApplication.run(MyOceanApplication.class, args);
     }
 }
