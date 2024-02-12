8b70278fa9f93640c5a98a7ea6cea8dd00d9310b
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-02-12 23:42:28 +0900

    Update MyOceanApplicationTests.java

--- src/test/java/dnd/myOcean/MyOceanApplicationTests.java
+++ src/test/java/dnd/myOcean/MyOceanApplicationTests.java
@@ -3,7 +3,7 @@ package dnd.myOcean;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class MyOceanApplicationTests {
 
 	@Test
