6003aad2b60129f2b97b84bdcb1cecad5a7c0f44
Dongwoongkim <rlaehddnd0422@naver.com>
2024-02-26 18:20:55 +0900

    refactor : 알림 이메일 내용 수정

--- src/main/java/dnd/myOcean/letter/alarm/AlarmService.java
+++ src/main/java/dnd/myOcean/letter/alarm/AlarmService.java
@@ -1,10 +1,12 @@
 package dnd.myOcean.letter.alarm;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,20 +16,20 @@ public class AlarmService {
     @Autowired
     private final JavaMailSender javaMailSender;
 
-    public void alarmLetterReceived(List<String> receiversEmail) {
-        String msg = "";
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setSubject("\uD83C\uDF0A새로운 편지가 흘러왔어요 \uD83D\uDC8C");
-        message.setText(new StringBuffer()
-                .append("<h1>내 마음 속 바다</h1><br/>")
-                .append("<p>새로운 편지가 흘러왔어요</p><br/>")
-                .append("<p>자세한 내용은 내 마음속 바다에서 확인해주세요</p><br/>")
-                .append("<a href=\"https://sea-of-my-heart.vercel.app/\"> 내 마음 속 바다 바로가기 </a>")
-                .toString());
-        
-        receiversEmail.forEach(receiverEmail -> {
-            message.setTo(receiverEmail);
+    public void alarmLetterReceived(List<String> receiversEmail) throws MessagingException {
+        for (String receiverEmail : receiversEmail) {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
+
+            helper.setTo(receiverEmail);
+            helper.setSubject("\uD83C\uDF0A새로운 편지가 흘러왔어요. \uD83D\uDC8C");
+
+            String msg = "<h1> \uD83C\uDF0A 새로운 편지가 흘러왔어요. \uD83D\uDC8C </h1>"
+                    + "<br> 자세한 내용은 내 마음속 바다에서 확인해주세요. <br/>"
+                    + "\uD83D\uDC49 내 마음 속 바다 <a href=\"https://sea-of-my-heart.vercel.app/\"> 바로가기 </a>";
+
+            helper.setText(msg, true);
             javaMailSender.send(message);
-        });
+        }
     }
 }
