d72558b5a44a7f9ae66ce294807c66b91830eae1
Dongwoongkim <rlaehddnd0422@naver.com>
2024-03-19 06:23:10 +0900

    refactor : 신고 유형 이름 변경

--- src/main/java/dnd/myOcean/report/domain/ReportType.java
+++ src/main/java/dnd/myOcean/report/domain/ReportType.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum ReportType {
-    
-    ABUSE, SHARING_PERSONAL_INFO, SEXUAL_CONTENT, ILLEGAL_CONTENT
+
+    ABUSE, ADVERTISE, SEXUAL_CONTENT, ILLEGAL_CONTENT
 }
